9ec0c231ecbf243af26bcc5965303f394adf5f8b
Jason Navi <jnavi1002@gmail.com>
2017-02-15 18:23:59 -0500

    Add files via upload

---
+++ TowersOfHanoi.cpp
@@ -0,0 +1,68 @@
+/**
+Name: Jason Navi
+Program: Towers of Hanoi with n rings
+Date: 3/19/16
+Program Details: calculates the total number of moves it would take to move a  certain amount of rings supplied by the user and prints out each individual ring movement
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+   //create an array of 3 vectory integer objects, each representing a tower
+   vector<int> t[3];
+
+   
+   //take in user input
+   int n;
+   cout << "Please enter the number of rings to move: ";
+   cin >> n;
+   
+   // close is the number of spots to move to the right to find the "closest" tower.
+   // If n is odd, close = 1 and far = 2.  If n is even, the reverse.
+   int close, far;
+   if(n % 2 == 1) {
+   	close = 1;
+   	far = 2;
+   }
+   else{
+   	close = 2;
+   	far = 1;
+   }
+   
+   //initialize the "from" tow
+   int from = 0, to = close, candidate = 1, move = 0;
+   cout << "\n";
+   
+   // Initialize the towers
+   for(int i = n + 1; i >= 1; --i)
+      t[0].push_back(i);
+   t[1].push_back(n+1);
+   t[2].push_back(n+1);
+   
+   // while t[1] does not contain all of the rings
+   while (t[1].size()<n+1) { 
+   	  //print out each individual ring movement from tower to tower
+      cout << "Move #" << ++move << ": Transfer ring " << candidate << " from tower " << char(from+'A') << " to tower " << char(to+'A') << "\n";
+      
+      // Move the ring from "from" to "to" (first copy it, then delete it from "from")
+      t[to].push_back(t[from].back());
+      t[from].pop_back();
+      
+      // from = the tower with the smallest ring that has not just been moved: (to+1)%3 or (to+2)%3
+      if (t[(to+1)%3].back() < t[(to+2)%3].back())
+         from = (to+1)%3;
+      else
+         from = (to+2)%3;
+         
+      // candidate = the ring on top of the from tower
+      candidate = t[from].back();
+         
+      // to = the closest tower on which the candidate can be placed: (from+close)%3 or (from+far)%3
+      if (t[(from + close) % 3].back() < t[from].back())
+         to = (from+far)%3;
+      else 
+         to = (from+close)%3; 
+   }
+   return 0;
+}
